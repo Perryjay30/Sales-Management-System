8077095aa6ec5e876d7fc2de7092297329087ddc
Perryjay30 <pelumijsh@gmail.com>
2024-06-19 10:59:12 +0100

    Logging service

--- src/main/java/com/challengetwo/salesmanagementsystem/SalesManagementSystemApplication.java
+++ src/main/java/com/challengetwo/salesmanagementsystem/SalesManagementSystemApplication.java
@@ -2,7 +2,6 @@ package com.challengetwo.salesmanagementsystem;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
 public class SalesManagementSystemApplication {
